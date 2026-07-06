4841768151a79611275408c4bd86db6c36d75fb7
RobinArtemstein <otu168x@hotmail.com>
2026-07-06 20:31:01 +0800

    The first commit on 6th July 2026.

---
+++ src/components/LightModeIcon.tsx
@@ -0,0 +1,20 @@
+export default function LightModeIcon() {
+    return(
+        <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11.5 18.4196V22.3878" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M11.5 0.612183V4.58044" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M4.58044 11.5H0.612183" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M22.3878 11.5H18.4195" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M14.9598 17.4926L16.9439 20.9292" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M6.05609 2.07092L8.0402 5.50748" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M5.50748 14.9597L2.07086 16.9439" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M20.9291 6.05609L17.4925 8.04023" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M17.4925 14.9597L20.9291 16.9439" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M2.07086 6.05609L5.50748 8.04023" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M8.0402 17.4926L6.05609 20.9291" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M16.9439 2.07092L14.9598 5.50754" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M11.5 15.7823C13.8651 15.7823 15.7823 13.8651 15.7823 11.5C15.7823 9.13496 13.8651 7.21771 11.5 7.21771C9.13496 7.21771 7.21771 9.13496 7.21771 11.5C7.21771 13.8651 9.13496 15.7823 11.5 15.7823Z" stroke="#FFFFFF" stroke-width="1   " stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+
+    )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,6 @@
+import LightModeIcon from "../components/LightModeIcon";
+import DarkModeIcon from "../components/DarkModeIcon";
+
 interface NavbarProps {
   isDarkMode: boolean;
   onToggleTheme: () => void;
@@ -17,13 +20,13 @@ export default function Navbar({ isDarkMode, onToggleTheme }: NavbarProps) {
       <button
         onClick={onToggleTheme}
         type="button"
-        className="px-4 py-2 rounded-lg bg-slate-100 dark:bg-slate-700 text-sm font-medium hover:bg-slate-200 dark:hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition-all"
+        className="px-4 py-2 rounded-xl bg-gray-100 dark:bg-gray-700 text-sm font-medium hover:bg-slate-200 dark:hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition-all"
         aria-label="Toggle Theme Mode"
       >
         {isDarkMode ? (
-          <span className="flex items-center gap-2">☀️ Light Mode</span>
+          <span className="flex items-center gap-2"><LightModeIcon /></span>
         ) : (
-          <span className="flex items-center gap-2">🌙 Dark Mode</span>
+          <span className="flex items-center gap-2"><DarkModeIcon /></span>
         )}
       </button>
     </nav>
